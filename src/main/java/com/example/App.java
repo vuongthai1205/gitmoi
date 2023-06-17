27244fc711e7b8c4538cd05e18ee03038644afc7
vuongthai1205 <2051010367vuong@ou.edu.vn>
2023-06-17 11:49:37 +0700

    da sua

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -25,7 +25,7 @@ public class App {
             // Tạo một câu lệnh SQL
 
 
-            String sql = "SELECT * FROM huy";
+            String sql = "SELECT * FROM huyhandsome";
 
 
             // Tạo đối tượng Statement để thực thi câu lệnh SQL
